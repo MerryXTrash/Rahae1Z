6e9d7676e1a80db606290f80122948c599f9af59
MerryXTrash <158755815+MerryXTrash@users.noreply.github.com>
2024-03-19 20:58:13 +0700

    Update Bloxfruit.lua

--- Bloxfruit.lua
+++ Bloxfruit.lua
@@ -1,1 +1,4 @@
+local Tween = game:GetService("TweenService")
 
+local Tw = Tween:Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(0.30, Enum.EasingStyle.Linear, Enum.EasingDirection.Out,0,false,0),
+{CFrame = CFrame.new(-289.21246337890625, 179.59799194335938, 372.76751708984375)}):Play()
